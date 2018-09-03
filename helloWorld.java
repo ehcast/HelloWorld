7f758fef6e34df48cec9831a26c96d6440d605dd
jsleh189 <31663739+jsleh189@users.noreply.github.com>
2018-09-03 12:45:32 +0800

    insert

---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+package helloWorld;
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("Phython just uses Print");
+		System.out.println("Clean & easy");
+	}
+}
